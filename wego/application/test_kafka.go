07baffd66c968e9e3bc8e9b04aaa4fe5aa8e2bd4
Zhangweiguo <zhangweiguo717@163.com>
2018-08-29 20:28:01 +0800

    kafka

---
+++ wego/application/test_kafka.go
@@ -0,0 +1,18 @@
+package main
+
+import "common/kafka"
+
+kafkaClient := kafka.KafkaClient{
+	Topic:         "KafkaTopicName",
+	BrokerList:    os.Getenv("KAFKA_PEERS"),
+	ConsumerGroup: "KafkaConsumerGroupName",
+	Zookeeper:     "127.0.0.1:8988,127.0.0.1:8986,127.0.0.1:8987",
+	Partitions:    "all",
+	BufferSize:    256,
+}
+kafkaClient.Init()
+kafkaClient.RunConsumer(func (msgFromKafka []byte) {
+	// Consume a Message from Kafka
+}, func (err error) {
+	// Get an Error When Consuming Kafka
+})
